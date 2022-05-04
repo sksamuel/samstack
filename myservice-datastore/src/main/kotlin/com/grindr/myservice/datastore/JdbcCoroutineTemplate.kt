c14d8d8c6db4c74ee6771c6b2a97f5b1d455d103
sksamuel <sam@sksamuel.com>
2022-05-04 17:13:30 -0500

    Added jdbc template and data source config

---
+++ myservice-datastore/src/main/kotlin/com/grindr/myservice/datastore/JdbcCoroutineTemplate.kt
@@ -0,0 +1,36 @@
+package com.grindr.myservice.datastore
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
+import javax.sql.DataSource
+
+class JdbcCoroutineTemplate(ds: DataSource) {
+
+   private val template = JdbcTemplate(ds)
+
+   suspend fun <T> query(
+      sql: String, mapper: RowMapper<T>,
+      args: List<Any?> = emptyList()
+   ): Result<T?> = runCatching {
+      withContext(Dispatchers.IO) {
+         template.query(sql, mapper, *args.toTypedArray()).firstOrNull()
+      }
+   }
+
+   suspend fun <T> queryForList(
+      sql: String, mapper: RowMapper<T>,
+      args: List<Any?> = emptyList()
+   ): Result<List<T>> = runCatching {
+      withContext(Dispatchers.IO) {
+         template.queryForStream(sql, mapper, *args.toTypedArray()).use { it.toList() }
+      }
+   }
+
+   suspend fun update(sql: String, args: List<Any?>): Result<Int> = runCatching {
+      withContext(Dispatchers.IO) {
+         template.update(sql, *args.toTypedArray())
+      }
+   }
+}
---
+++ myservice-datastore/src/main/kotlin/com/grindr/myservice/datastore/config.kt
@@ -0,0 +1,53 @@
+package com.grindr.myservice.datastore
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.micrometer.core.instrument.MeterRegistry
+import javax.sql.DataSource
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.hours
+
+data class DatabaseConfig(
+   val url: String,
+   val username: String,
+   val password: String,
+   val maximumPoolSize: Int = 8,
+   val minimumIdle: Int = 2,
+   val idleTimeout: Duration = 1.hours,
+   val maxLifetime: Duration = 1.hours,
+   val cachePrepStmts: Boolean = true,
+   val prepStmtCacheSize: Int = 1,
+   val prepStmtCacheSqlLimit: Int = 1,
+   val poolName: String?,
+)
+
+fun createDataSource(config: DatabaseConfig, registry: MeterRegistry): DataSource {
+   val hikariConfig = HikariConfig()
+
+   hikariConfig.jdbcUrl = config.url
+   hikariConfig.username = config.username
+   hikariConfig.password = config.password
+
+   // maximum number of connections
+   hikariConfig.maximumPoolSize = config.maximumPoolSize
+
+   // keep at least this number of connections open
+   hikariConfig.minimumIdle = config.minimumIdle
+
+   // default is 30 minutes
+   hikariConfig.maxLifetime = config.maxLifetime.inWholeMilliseconds
+
+   hikariConfig.idleTimeout = config.idleTimeout.inWholeMilliseconds
+
+   // these are properties on the underlying JDBC connection
+   hikariConfig.addDataSourceProperty("cachePrepStmts", config.cachePrepStmts)
+   hikariConfig.addDataSourceProperty("prepStmtCacheSize", config.prepStmtCacheSize)
+   hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", config.prepStmtCacheSqlLimit)
+
+   if (config.poolName != null) hikariConfig.poolName = config.poolName
+
+   // this wires in micrometer metrics
+   hikariConfig.metricRegistry = registry
+
+   return HikariDataSource(hikariConfig)
+}
